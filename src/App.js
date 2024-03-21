91e06e33c0fec15cfe55bc27529d3165eebd47a2
unknown <yusquen22@gmail.com>
2024-03-21 17:11:16 -0500

    TODOs MachineComponents

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import React from 'react';
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edita el archivo <code>src/App.js</code> y guarda para recargar.</p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-function TodoItem() {
-  return (
-    <li>
-      <span>V</span>
-      <p>Llorar con la Llorona</p>
-      <span>X</span>
-    </li>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+      
+      {/* <CreateTodoButton /> */}
+    </div>
   );
 }
 
