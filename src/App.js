cb8221caa16db8d9fa06511e7ad607249f342899
unknown <yusquen22@gmail.com>
2024-03-21 16:32:29 -0500

    first commit:First component TodoItem()

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import React from 'react';
 import logo from './platzi.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
+        <p>Edita el archivo <code>src/App.js</code> y guarda para recargar.</p>
         <a
           className="App-link"
           href="https://platzi.com/reactjs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
