b6ce12246159093cd0ed1d1a20587ecf0ff5a5db
unknown <yusquen22@gmail.com>
2024-03-25 18:08:37 -0500

    Events on React: OnChange and OnClick

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import './CreateTodoButton.css'; // Importa los estilos CSS
+import './CreateTodoButton.css';
 
 function CreateTodoButton() {
   return (
-    <button className="CreateTodoButton">+</button>
+    <button
+      className="CreateTodoButton"
+      onClick={
+        (event) => {
+          console.log('le diste click')
+          console.log(event)
+          console.log(event.target)
+        }
+      }
+    >+</button>
   );
 }
 
