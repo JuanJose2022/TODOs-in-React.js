5739f7e289994d83d8c1698545866d1e1fe965a7
unknown <yusquen22@gmail.com>
2024-03-29 11:32:29 -0500

    Loading and Error States. Updating States from useEffect. Loading Skeletons.

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,9 +2,14 @@ import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI({
+  loading,
+  error,
   completedTodos,
   totalTodos,
   searchValue,
@@ -25,6 +30,16 @@ function AppUI({
       />
 
       <TodoList>
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
+        )}
+        {error && <TodosError/>}
+        {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
+
         {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
