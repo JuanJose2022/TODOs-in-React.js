009f48d95207fbbcf6b068148d2b5a2f9fa59d10
unknown <yusquen22@gmail.com>
2024-08-03 16:58:27 -0500

    Changing version to 17.0.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = document.getElementById('root');
+render(<App />, root);
+
+
